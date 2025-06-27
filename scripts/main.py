4df9b1444f5e0373989f9cd34e4f1313676faae4
Emily Su <emilyzqsu@gmail.com>
2025-06-27 12:28:48 -0400

    Added in gpqa dataset to main

--- scripts/main.py
+++ scripts/main.py
@@ -1,19 +1,24 @@
 from datasets import load_dataset
 from benchmark_update import benchmark_update
 
-def main(benchmark_dataset):
-    """This function begins process of replacing values in benchmark dataset,
+def main(benchmark_dataset, gpqa_dataset):
+    """This function begins process of replacing values in <benchmark dataset>,
     automating prompting of different GPT models to answer the prompts 
-    in the benchmark dataset, and having the same GPT model evaluate its own response. 
-    Using all this information, output a CSV file. 
+    in the benchmark dataset and <gpqa_dataset> dataset, and having the same GPT model 
+    evaluate its own response. Using all this information, output a CSV file. 
     """
-    training_set = benchmark_dataset["train"]
-    testing_set = benchmark_dataset["test"]
+    training_set_benchmark = benchmark_dataset["train"]
+    testing_set_benchmark = benchmark_dataset["test"]
+    training_set_gpqa_dataset = gpqa_dataset["train"]
+    testing_set_gpqa_dataset = gpqa_dataset["test"]
+    
     # Replace values in benchmark dataset 
-    benchmark_update(training_set, testing_set)
+    benchmark_update(training_set_benchmark, testing_set_benchmark)
     
 if __name__ == "__main__":
-    # Load in dataset
+    # Load in datasets
     benchmark_dataset = load_dataset("openai/gsm8k", "main")
+    gpqa_dataset = load_dataset("Idavidrein/gpqa", "gpqa_diamond") #
+    
     # Pass in dataset to main function
-    main(benchmark_dataset)
+    main(benchmark_dataset, gpqa_dataset)
