3817775ce22aecd4a2b4b27bd65f603591af54fd
Emily Su <emilyzqsu@gmail.com>
2025-06-27 13:15:53 -0400

    Updated datasets in main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,8 @@
 from datasets import load_dataset
 from benchmark_update import benchmark_update
+from huggingface_hub import login
+import os
+from dotenv import load_dotenv
 
 def main(benchmark_dataset, gpqa_dataset):
     """This function begins process of replacing values in <benchmark dataset>,
@@ -7,18 +10,19 @@ def main(benchmark_dataset, gpqa_dataset):
     in the benchmark dataset and <gpqa_dataset> dataset, and having the same GPT model 
     evaluate its own response. Using all this information, output a CSV file. 
     """
-    training_set_benchmark = benchmark_dataset["train"]
-    testing_set_benchmark = benchmark_dataset["test"]
-    training_set_gpqa_dataset = gpqa_dataset["train"]
-    testing_set_gpqa_dataset = gpqa_dataset["test"]
-    
+    benchmark_dataset = benchmark_dataset["train"]
+    gpqa_dataset = gpqa_dataset["train"]
+
     # Replace values in benchmark dataset 
-    benchmark_update(training_set_benchmark, testing_set_benchmark)
+    benchmark_update(benchmark_dataset)
     
 if __name__ == "__main__":
+    # Load in .env file 
+    load_dotenv()
+    # Log into hugging face and pass it YOUR access token from .env file 
+    login(token = os.getenv('HUGGING_FACE_ACCESS_TOKEN'))
     # Load in datasets
     benchmark_dataset = load_dataset("openai/gsm8k", "main")
-    gpqa_dataset = load_dataset("Idavidrein/gpqa", "gpqa_diamond") #
-    
+    gpqa_dataset = load_dataset("Idavidrein/gpqa", "gpqa_diamond")
     # Pass in dataset to main function
     main(benchmark_dataset, gpqa_dataset)
