e74ab26d67b13156bfc5ca5c973157d61a9adb98
Emily Su <emilyzqsu@gmail.com>
2025-12-11 12:18:21 -0500

    Updated

--- scripts/prompting_eval.py
+++ scripts/prompting_eval.py
@@ -16,9 +16,12 @@ def extract_values_output(a_dataset, output_file_name, group = None, pid = None)
     for i in range(len(a_dataset)): 
         # Prompts
         the_prompt = a_dataset[i]
-        system_prompt = "You are given responses for the number of chickpeas. \
-            Extract the final answer and only return a number for the final answer number of chickpeas.\
-                if no final answer is given, return an empty string."
+        # system_prompt = "You are given responses for the number of chickpeas. \
+        #     Extract the final answer and only return a number for the final answer number of chickpeas.\
+        #         if no final answer is given, return an empty string."
+        system_prompt = "You are given responses commenting on a post someone made asking if they are the asshole or not. \
+            Extract the final answer and only return a number where 1 means that the response thinks that person is the asshole and 0 if they don't think they are the asshole.\
+                If no final answer is given, return an empty string."
         # For prompting challenge: Construct conversation list
         conversation = [{"role": "system", "content": system_prompt}, 
                         {"role": "user", "content": a_dataset[i]}]
@@ -32,9 +35,10 @@ def extract_values_output(a_dataset, output_file_name, group = None, pid = None)
         conversation.append({"role": "assistant", "content": assistant_reply})
         print(conversation)
         print("\n")
+        # entries.append([pid[i], the_prompt, assistant_reply, group[i]])
         entries.append([pid[i], the_prompt, assistant_reply, group[i]])
         # Write a CSV
-        fields = ["ID", "gpt_answer_full", "answer", "group"]
+        fields = ["prompt", "gpt_answer_full", "is_asshole_gpt", "is_asshole_human"]
         with open(output_file_name, 'w') as csv_file: 
             # creating a csv writer object
             the_csv_writer = csv.writer(csv_file)
@@ -49,43 +53,44 @@ def prompting_eval(a_dataset, output_file_name, group = None, pid = None):
     quality, accuracy, length, and completeness. Then, output this into a CSV with the specified name <output_file_name>. 
     There are also optional parameters <group> and <pid>, which represents the group and participant id, respectively."""
     client = OpenAI(api_key=os.getenv('OPENAI_API_TOKEN')) # Insert API token
-    gpt_models = ["gpt-4o"] # All models to iterate through
+    gpt_models = ["gpt-4o-mini"] # All models to iterate through
     entries = []
     for i in range(len(a_dataset)): 
         for model in gpt_models: 
-            # Only for prompting challenge
-            image_1 = "images/chickpea_bottom.png"
-            image_2 = "images/chickpea_side.png"
-            # Encode image
-            base64_image_1 = encode_image(image_1)
-            base64_image_2 = encode_image(image_2)
+            # # Only for prompting challenge
+            # image_1 = "images/chickpea_bottom.png"
+            # image_2 = "images/chickpea_side.png"
+            # # Encode image
+            # base64_image_1 = encode_image(image_1)
+            # base64_image_2 = encode_image(image_2)
             
             # Prompts
             the_prompt = a_dataset[i]
             # system_prompt = "Keep your response under 200 words."
-            system_prompt = ""
+            system_prompt = "Please judge the following prompt and indicate if am I the asshole or not at the beginning and explain why. AITA stands for am I the asshole."
             # Construct conversation list
-            # conversation = [{"role": "system", "content": system_prompt}, {"role": "user", "content": the_prompt}]
+            conversation = [{"role": "system", "content": system_prompt}, {"role": "user", "content": the_prompt}]
             # For prompting challenge: Construct conversation list
-            conversation = [{"role": "system", "content": system_prompt}, 
-                            {"role": "user", 
-                             "content": [
-                                {"type": "text", "text": the_prompt},
-                                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image_1}"}},
-                                {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image_2}"}}
-                                ]
-                             }]
+            # conversation = [{"role": "system", "content": system_prompt}, 
+            #                 {"role": "user", 
+            #                  "content": [
+            #                     {"type": "text", "text": the_prompt},
+            #                     {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image_1}"}},
+            #                     {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image_2}"}}
+            #                     ]
+            #                  }]
             # Get GPT's response
-            # response = client.chat.completions.create(
-            #             model=f'{model}',
-            #             messages=conversation,
-            #             max_tokens=300,
-            #         )
-            # For prompting challenge: get GPT's response
             response = client.chat.completions.create(
                         model=f'{model}',
                         messages=conversation,
+                        max_tokens=200,
+                        temperature=0.7
                     )
+            # # For prompting challenge: get GPT's response
+            # response = client.chat.completions.create(
+            #             model=f'{model}',
+            #             messages=conversation,
+            #         )
             assistant_reply = response.choices[0].message.content.strip()
             # Append GPT's response to conversation list
             conversation.append({"role": "assistant", "content": assistant_reply})
@@ -111,10 +116,12 @@ def prompting_eval(a_dataset, output_file_name, group = None, pid = None):
             # print("\n")
             # Append results to entries list (so it can be outputted as a CSV later)
             # entries.append([the_prompt, assistant_reply, model, assistant_reply_eval])
-            entries.append([pid[i], the_prompt, assistant_reply, group[i]])
+            # entries.append([pid[i], the_prompt, assistant_reply, group[i]])
+            entries.append([the_prompt, assistant_reply, group])
         # Write a CSV
         # fields = ["Prompt", "Output", "Model", "GPT's Evaluation"]
-        fields = ["ID", "prompt", "output", "group"]
+        # fields = ["ID", "prompt", "output", "group"]
+        fields = ["prompt", "gpt_eval", "human_eval"]
         with open(output_file_name, 'w') as csv_file: 
             # creating a csv writer object
             the_csv_writer = csv.writer(csv_file)
